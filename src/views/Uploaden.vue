9da33d124703ded41f3e2837b71b8517da10600a
LadyoftheCave <75953681+LadyoftheCave@users.noreply.github.com>
2024-02-19 12:49:16 +0100

    19-02
    upload images

--- src/views/Uploaden.vue
+++ src/views/Uploaden.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-      <img src="../assets/images/image.jpg" alt="Grote header foto" />
+      <section class="hero-section uploaden">
+                <h1 class="interior">Uploaden</h1>
+            </section>
+
+            <picture>
+              <source media="(max-width: 320px)" srcset="images/header_image_medium_sv_upload.webp" type="image/webp">
+              <source media="(min-width: 321px) and (max-width: 1200px)" srcset="images/header_image_medium_sv_upload.webp" type="image/webp">
+              <source media="(min-width: 1201px)" srcset="images/interior_header.webp" type="image/webp">
+              <img src="images/interior_header.jpg" alt="Header Banner" class="header-image">
+            </picture>
       <p>Je tekst voor uploadenpagina hier</p>
     </div>
   </template>
